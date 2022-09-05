f933e75c4091a4dbdb43b6dadf2878420b09a27b
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-05 16:36:06 -0300

    created: docker n docker compose

--- anchor/settings.py
+++ anchor/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import dotenv
 
+dotenv.load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-c2e^-9!29ydxr&6uam-ad3xllnb^9!8*p&#4zzm%4xtf5%uk)-"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
