e5a33b03cd0747bf04afe59ec8e886c141c751e7
Amanda Oliveira <amandaolits@gmail.com>
2022-09-06 11:56:58 -0300

    create: zone model + serializer

--- zones/models.py
+++ zones/models.py
@@ -1,9 +1,11 @@
+import uuid
+
 from django.db import models
 
-class Zone(models.Model):
-  name = models.CharField(max_length=50)
-  total_selled_tickets = models.IntegerField()
-  is_active = models.BooleanField(default=False)
-  created_at = models.DateTimeField(auto_now_add=True)
 
-  # event = models.ForeignKey('events.Event', on_delete=models.CASCADE, related_name='zones')
+class Zone(models.Model):
+  id                   = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+  name                 = models.CharField(max_length=50)
+  total_selled_tickets = models.IntegerField()
+  is_active            = models.BooleanField(default=False)
+  created_at           = models.DateTimeField(auto_now_add=True)
