6929753a507bc1053dfb55b58cedcc9eabe40681
Amanda Oliveira <amandaolits@gmail.com>
2022-09-06 11:00:13 -0300

    create: zones model

--- zones/models.py
+++ zones/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Zone(models.Model):
+  name = models.CharField(max_length=50)
+  total_selled_tickets = models.IntegerField()
+  is_active = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  # event = models.ForeignKey('events.Event', on_delete=models.CASCADE, related_name='zones')
