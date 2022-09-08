89d7df11d8d691fffce0edf334a5a9998e31a04e
paulo <paulodgsouza@gmail.com>
2022-09-08 13:38:25 -0300

    feat: add ticket views and serializers

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from tickets.models import Ticket
+from tickets.serializers import TicketSerializer
+
+class TicketView(generics.ListCreateAPIView):
+
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class TicketRetrieveView(generics.RetrieveAPIView):
+
+    lookup_url_kwarg = "user_id"
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+class TicketDeleteView(generics.DestroyAPIView):
+
+    lookup_url_kwarg = "ticket_id"
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
