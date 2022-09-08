5b7d2a5745b4cb96937ad3b329af39a89c497766
paulo <paulodgsouza@gmail.com>
2022-09-08 14:50:58 -0300

    refactor: change url name

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from tickets.views import TicketDeleteView, TicketView, TicketRetrieveView
 
 urlpatterns = [
-    path("ticket/", TicketView.as_view()),
-    path("ticket/<user_id>/", TicketRetrieveView.as_view()),
+    path("tickets/", TicketView.as_view()),
+    path("tickets/<user_id>/", TicketRetrieveView.as_view()),
     path("tickets/<ticket_id/", TicketDeleteView.as_view())
 ]
