b5f02f122e6527f0d03f53423ed973df8ff09db2
paulo <paulodgsouza@gmail.com>
2022-09-08 11:50:05 -0300

    refactor: update name of model-Ticket

--- tickets/models.py
+++ tickets/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from django.db import models
 
 
-class ticket(models.Model):
+class Ticket(models.Model):
     id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="tickets"
