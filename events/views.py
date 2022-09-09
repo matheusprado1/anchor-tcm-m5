990b2ddde066a7b6dd73da97cc9efa33724eab9a
matheusprado1 <matheusprado1096@gmail.com>
2022-09-09 11:47:34 -0300

    feat: conflict

--- events/views.py
+++ events/views.py
@@ -2,11 +2,11 @@ from django_filters import rest_framework as filters
 from rest_framework import generics
 
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from events.filters import DistanceFilter
 
-from .mixins import SerializerByMethod
+from .mixins import SerializerByMethodMixin
 from .models import Event
 from .serializers import (
     EventDetailSerializer,
@@ -15,29 +15,29 @@ from .serializers import (
 )
 
 
-class EventView(SerializerByMethod, generics.ListCreateAPIView):
+class EventView(SerializerByMethodMixin, generics.ListCreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     queryset = Event.objects.all()
     serializer_map = {"GET": EventSerializer, "POST": EventDetailSerializer}
 
-  def perform_create(self, serializer):
-    serializer.save(address=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(address=self.request.user)
 
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
-  authentication_classes = [TokenAuthentication]
-  permission_classes = [IsAuthenticatedOrReadOnly]
-  
-  queryset = Event.objects.all()
-  serializer_class = EventDetailSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Event.objects.all()
+    serializer_class = EventDetailSerializer
 
 
 class EventDistanceView(generics.ListAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]  # ajustar aqui
-    
+
     queryset = Event.objects.all()
     serializer_class = EventDistanceSerializer
     filter_backends = (filters.DjangoFilterBackend,)
