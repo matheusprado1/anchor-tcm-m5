79ea97d4004d6fa2e75f99e52f2ce72569a87f54
Amanda Oliveira <amandaolits@gmail.com>
2022-09-11 18:09:40 -0300

    feat: added zone permissions

--- zones/views.py
+++ zones/views.py
@@ -1,29 +1,38 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from rest_framework.views import APIView, Request, Response, status
 
 from zones.models import Zone
+from zones.permissions import (IsOwner, IsSuperuser,
+                               IsSuperuserOrAuthenticatedToCreate, IsUser)
 from zones.serializers import ZoneSerializer
 
 
 class ZoneView(generics.ListCreateAPIView):
+  permission_classes = [IsSuperuserOrAuthenticatedToCreate]
+  
   serializer_class = ZoneSerializer
   queryset = Zone.objects.all()
 
 
 class ZoneDetailView(generics.RetrieveAPIView):
+  permission_classes = [IsSuperuser | IsUser]
+
   serializer_class = ZoneSerializer
   queryset = Zone.objects.all()
 
   lookup_url_kwarg = "zone_id"
 
+
 class ZoneDetailView(generics.UpdateAPIView):
+  permission_classes = [IsSuperuser | IsOwner]
+
   serializer_class = ZoneSerializer
   queryset = Zone.objects.all()
 
   lookup_url_kwarg = "zone_id"
 
 class ZoneDetailView(generics.DestroyAPIView):
+  permission_classes = [IsSuperuser | IsOwner]
+
   serializer_class = ZoneSerializer
   queryset = Zone.objects.all()
 
