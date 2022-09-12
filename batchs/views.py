09521bb1e5260c19cce295ef8fb37176bd2642de
matheusprado1 <matheusprado1096@gmail.com>
2022-09-12 11:23:20 -0300

    feat: added permissions

--- batchs/views.py
+++ batchs/views.py
@@ -1,18 +1,21 @@
 from rest_framework import generics
-from batchs.mixins import SerializerByMixin
+from rest_framework.authentication import TokenAuthentication
 
 from batchs.models import Batch
 from batchs.serializers import BatchDetailSerializer, BatchSerializer
+from batchs.permissions import OwnerOrSuperUserAuth, SuperUserAuth
 
-
-class BatchsView(SerializerByMixin, generics.ListCreateAPIView):
+class BatchsView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [OwnerOrSuperUserAuth]
     queryset = Batch.objects.all()
-    serializer_map = {"GET": BatchSerializer,
-                      "POST": BatchSerializer}
+    serializer_class = BatchSerializer
+    lookup_field = "batch_id"
 
 
-
-class UpdateBatchsView(SerializerByMixin, generics.UpdateAPIView):
+class UpdateBatchsView(generics.RetrieveUpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [OwnerOrSuperUserAuth]
     queryset = Batch.objects.all()
-    serializer_map = {"PATCH": BatchDetailSerializer}
-
+    serializer_class = BatchDetailSerializer
+    lookup_field = "batch_id"
