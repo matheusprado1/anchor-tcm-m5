bb4119aa002e17977ed9244ce5a418eb41c72590
matheusprado1 <matheusprado1096@gmail.com>
2022-09-12 15:09:21 -0300

    feat: comment photo

--- addresses/tests/test_model.py
+++ addresses/tests/test_model.py
@@ -11,7 +11,7 @@ class ProductModelTest(TestCase):
         cls.address_1 = baker.make("Address")
         cls.address_2 = baker.make("Address")
         cls.user_1 = baker.make("users.User", address=cls.address_1)
-        cls.event_1 = baker.make("Event", address=cls.address_2)
+        # cls.event_1 = baker.make("Event", address=cls.address_2)
 
     def test_one_to_one_relationship_is_made(self):
         print("executando test_one_to_one_relationship_is_made")
