5b2053bf8d44e405a14f42bf107fe47465e5343c
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-12 13:08:58 -0300

    adjusted: permission

--- addresses/permissions.py
+++ addresses/permissions.py
@@ -13,16 +13,3 @@ class SuperUserAuth(permissions.BasePermission):
             and request.user.is_superuser
         )
 
-
-class OwnerOrSuperUserAuth(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj: Address):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        # if request.method in permissions.SAFE_METHODS:
-        #     return True
-
-        # Instance must have an attribute named `owner`.
-        import ipdb
-
-        ipdb.set_trace()
-        return obj.user == request.user
