3a627e917b6d0118a40776dd2b7acd7395723077
Amanda Oliveira <amandaolits@gmail.com>
2022-09-12 15:18:21 -0300

    refactor: routes working

--- zones/views.py
+++ zones/views.py
@@ -1,38 +1,15 @@
 from rest_framework import generics
 
 from zones.models import Zone
-from zones.permissions import (IsOwner, IsSuperuser,
-                               IsSuperuserOrAuthenticatedToCreate, IsUser)
 from zones.serializers import ZoneSerializer
 
 
 class ZoneView(generics.ListCreateAPIView):
-  permission_classes = [IsSuperuserOrAuthenticatedToCreate]
-  
   serializer_class = ZoneSerializer
   queryset = Zone.objects.all()
 
 
-class ZoneDetailView(generics.RetrieveAPIView):
-  permission_classes = [IsSuperuser | IsUser]
-
-  serializer_class = ZoneSerializer
-  queryset = Zone.objects.all()
-
-  lookup_url_kwarg = "zone_id"
-
-
-class ZoneDetailView(generics.UpdateAPIView):
-  permission_classes = [IsSuperuser | IsOwner]
-
-  serializer_class = ZoneSerializer
-  queryset = Zone.objects.all()
-
-  lookup_url_kwarg = "zone_id"
-
-class ZoneDetailView(generics.DestroyAPIView):
-  permission_classes = [IsSuperuser | IsOwner]
-
+class ZoneDetailView(generics.RetrieveUpdateDestroyAPIView):
   serializer_class = ZoneSerializer
   queryset = Zone.objects.all()
 
