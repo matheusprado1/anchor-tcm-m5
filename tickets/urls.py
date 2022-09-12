db61b2c30d348f61ec7e827e6db23eabb0856b0a
paulo <paulodgsouza@gmail.com>
2022-09-12 20:17:04 -0300

    fix: correct tickets urls

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from tickets.views import TicketDeleteView, TicketView, TicketRetrieveView
+from tickets.views import TicketView, UserTicketsView, TicketDeleteView
 
 urlpatterns = [
     path("tickets/", TicketView.as_view()),
-    path("tickets/user/<user_id>/", TicketRetrieveView.as_view()),
+    path("tickets/user/<user_id>/", UserTicketsView.as_view()),
     path("tickets/<ticket_id>/", TicketDeleteView.as_view())
 ]
