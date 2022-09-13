f0fb5f8cd3b2287ee433d0f0eb06959bbd3b4b08
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-13 13:48:20 -0300

    feature: total sold tickets

--- zones/serializers.py
+++ zones/serializers.py
@@ -4,6 +4,12 @@ from .models import Zone
 
 
 class ZoneSerializer(serializers.ModelSerializer):
+    total_sold_tickets = serializers.SerializerMethodField()
+
     class Meta:
         model = Zone
         fields = "__all__"
+        read_only_fields = ["total_sold_tickets"]
+
+    def get_total_sold_tickets(self, obj):
+        return obj.get_sold_tickets()
