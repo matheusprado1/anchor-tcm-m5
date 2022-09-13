87f60d3b99fadc925eec5ebb1378fcefe07f8c01
paulo <paulodgsouza@gmail.com>
2022-09-13 15:21:37 -0300

    t7

--- anchor/settings.py
+++ anchor/settings.py
@@ -106,12 +106,8 @@ WSGI_APPLICATION = "anchor.wsgi.application"
 if os.getenv("GITHUB_WORKFLOW"):
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "github-actions",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "localhost",
-            "PORT": "5432",
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
     # SECRET_KEY = "sua secret_key" #não esqueça de alterar
