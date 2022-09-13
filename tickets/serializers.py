2bb1e4f316489afff211db91a507f0679a587e49
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-13 12:32:12 -0300

    feature: validation age, date, tickets added on ticket creation

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,18 +1,16 @@
-from rest_framework import serializers
-from rest_framework.serializers import UUIDField
-from tickets.models import Ticket
 from batchs.models import Batch
 from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+from rest_framework.serializers import UUIDField
+
+from tickets.models import Ticket
 
 
 class TicketSerializer(serializers.ModelSerializer):
 
     batch_id = UUIDField()
+
     class Meta:
         model = Ticket
         fields = ["id", "user_id", "batch_id", "created_at"]
-
-
-    def create(self, validated_data):
-        user = self.context["request"].user
-        return Ticket.objects.create(**validated_data, user=user)
+        extra_kwargs = {"qnt_tickets": {"write_only": True}}
--- tickets/views.py
+++ tickets/views.py
@@ -1,24 +1,68 @@
-from rest_framework import generics
-from django.shortcuts import get_object_or_404
+from batchs.erros import (
+    AgeValidationError,
+    DataValidationError,
+    TicketValidationError,
+)
 from batchs.models import Batch
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.views import APIView, Request, Response, status
 
-from tickets.permissions import IsSuperuserOrAuthenticatedToCreate, IsSuperuser, IsUser, IsSuperuserOrIsOwner
 from tickets.models import Ticket
+from tickets.permissions import (
+    IsSuperuser,
+    IsSuperuserOrAuthenticatedToCreate,
+    IsSuperuserOrIsOwner,
+    IsUser,
+)
 from tickets.serializers import TicketSerializer
 
+
 class TicketView(generics.ListCreateAPIView):
-
     permission_classes = [IsSuperuserOrAuthenticatedToCreate]
-
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
 
-    def perform_create(self, serializer):
-        get_object_or_404(Batch, pk=serializer.validated_data["batch_id"])
-        serializer.save()
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        batch = get_object_or_404(
+            Batch, pk=serializer.validated_data["batch_id"]
+        )
+
+        user = self.request.user
+        try:
+            if user.age() < batch.zone.event.full_age:
+                raise AgeValidationError
+            if batch.is_date_valid() is False:
+                raise DataValidationError
+            if batch.is_enough_tickets() is False:
+                raise TicketValidationError
+
+        except AgeValidationError:
+            return Response(
+                {"detail": "Not old enought to buy ticket"},
+                status.HTTP_403_FORBIDDEN,
+            )
+        except DataValidationError:
+            return Response(
+                {"detail": "Batch over due date"}, status.HTTP_403_FORBIDDEN
+            )
+        except TicketValidationError:
+            return Response(
+                {"detail": "Not enought tickets on this batch"},
+                status.HTTP_403_FORBIDDEN,
+            )
+
+        serializer.save(batch=batch, user=user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
 
 class UserTicketsView(generics.ListAPIView):
-
     permission_classes = [IsSuperuser | IsUser]
 
     queryset = Ticket.objects.all()
@@ -26,6 +70,7 @@ class UserTicketsView(generics.ListAPIView):
 
     def get_queryset(self):
         return self.queryset.filter(user_id=self.kwargs["user_id"])
+
 
 class TicketDeleteView(generics.DestroyAPIView):
 
