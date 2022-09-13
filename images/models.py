35facf9c2df120418d891b2b9aa9b7dd8088445f
paulo <paulodgsouza@gmail.com>
2022-09-13 13:27:57 -0300

    fix: linting in models

--- images/models.py
+++ images/models.py
@@ -7,13 +7,17 @@ from cloudinary.models import CloudinaryField
 MB = 1
 MAX_SIZE = MB * 1024 * 1024
 
+
 def validate_file_size(file):
-  if file.size > MAX_SIZE:
-    raise ValidationError("File exced maximum size 1MB")
+    if file.size > MAX_SIZE:
+        raise ValidationError("File exced maximum size 1MB")
+
 
 class Image(models.Model):
-  id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-  photo = CloudinaryField(validators=[validate_file_size])
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    photo = CloudinaryField(validators=[validate_file_size])
 
-  user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="images", default="", null=True)
-  event = models.ForeignKey("events.Event", on_delete=models.CASCADE, related_name="images", default="", null=True)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE,
+                             related_name="images", default="", null=True)
+    event = models.ForeignKey("events.Event", on_delete=models.CASCADE,
+                              related_name="images", default="", null=True)
