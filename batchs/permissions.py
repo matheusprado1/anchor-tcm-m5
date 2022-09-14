1cc84572f56a1194cb31217848224c42bff21ec0
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-14 15:05:31 -0300

    :bug: fix: zone get object or 404

--- batchs/permissions.py
+++ batchs/permissions.py
@@ -2,6 +2,8 @@ from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 from rest_framework.views import Request, View
 from zones.models import Zone
+from django.shortcuts import get_object_or_404
+
 
 from batchs.models import Batch
 
@@ -19,5 +21,5 @@ class IsOwner(BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        zone = Zone.objects.get(id=request.data["zone"])
+        zone = get_object_or_404(Zone,id=request.data["zone"])
         return zone.event.user == request.user
