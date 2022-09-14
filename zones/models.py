53b2a2a03ee75bf1126e73da6479baf0f931e330
talitta-nunes <70520439+talitta-nunes@users.noreply.github.com>
2022-09-14 11:54:47 -0300

    feat: email config + model user test

--- zones/models.py
+++ zones/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 
 class Zone(models.Model):
-  id                   = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-  name                 = models.CharField(max_length=50)
-  total_selled_tickets = models.IntegerField()
-  is_active            = models.BooleanField(default=False)
-  created_at           = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=50)
+    total_selled_tickets = models.IntegerField()
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-  event = models.ForeignKey("events.Event", on_delete=models.CASCADE, related_name="zones")
+    event = models.ForeignKey(
+        "events.Event", on_delete=models.CASCADE, related_name="zones"
+    )
