d7a3f5cac3bca945d23c0ab053ac77669a5fa848
matheusprado1 <matheusprado1096@gmail.com>
2022-09-15 08:53:52 -0300

    fix: debug=true

--- anchor/settings.py
+++ anchor/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = (
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["anchor-tcm-m5.herokuapp.com/", "localhost"]
 
